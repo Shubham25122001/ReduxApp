ab2a74a5f5d8779911fe088589ac5d835722e4da
shubham <shubham25122001@gmail.com>
2023-02-26 19:30:17 +0530

    Lecture 74

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shope';
+
+function App() {
+  return (
+    <>
+     <Navbar />
+    <div className="container">
+      <Shop/>
+    </div>
+
+    </>
+  );
+}
+
+export default App;
